3db4cb405f37066ad81635816a44d3cf49303780
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-12 17:43:20 -0400

    Add users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :events, only: %i[index show new create]
+  resources :users, only: %i[show]
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
