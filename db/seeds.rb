ede1aaf5d03e6c12246b1d5f7229e4f723c69435
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-11 22:26:20 -0400

    Update seed script to use faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,44 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'faker'
+
+# 1 will be Foo.
+USER_COUNT = 20
+EVENT_COUNT = 10
+ATTENDANCES_COUNT = USER_COUNT * 2
+
+# Seed users
 foo = User.create(
   username: 'Foo Bar',
   email: 'foo@bar.com',
   password: '123456'
 )
 
-foo.hosted_events.create(
-  name: 'First Event',
-  description: 'lorem ipsum',
-  location: 'my house',
-  date: DateTime.now
-)
+2.upto USER_COUNT do
+  name = Faker::Name.name
+  User.create(
+    username: name,
+    email: Faker::Internet.free_email(name: name),
+    password: 'password'
+  )
+end
+
+# Seed Events
+EVENT_COUNT.times do
+  Event.create(
+    host_id: rand(1..USER_COUNT),
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    location: Faker::Address.full_address,
+    date: Faker::Date.forward
+  )
+end
+
+# Seed Attendances
+ATTENDANCES_COUNT.times do
+  Attendance.create(
+    user_id: rand(1..USER_COUNT),
+    event_id: rand(1..EVENT_COUNT)
+  )
+end
