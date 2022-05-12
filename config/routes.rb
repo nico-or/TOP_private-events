627cf8bff2f3818dc3ab0a8b4bb76de818d7bc63
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-12 17:56:26 -0400

    Fix signout routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   resources :events, only: %i[index show new create]
   resources :users, only: %i[show]
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
 
   root to: "events#index"
 end
