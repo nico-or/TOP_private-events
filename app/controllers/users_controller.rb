2348fb512566f93f7304cbcc8069071865f96258
Nicolás Órdenes <nicordenes@gmail.com>
2022-05-19 12:20:39 -0400

    Add eager loading to users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :set_user, only: :show
 
   def show
+    @hosted_events = @user.hosted_events.records
+    @future_events = @user.attending_events.future.records
+    @past_events = @user.attending_events.past.records
   end
 
   private
